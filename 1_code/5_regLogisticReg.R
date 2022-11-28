8f8cfedb1358922e5e00f3e57269493bebd6ba7c
Nikki Freeman <nlbfreeman@gmail.com>
2022-11-28 17:15:49 -0500

    Rm race_asian from svm analysis, update analysis notebook

--- 1_code/5_regLogisticReg.R
+++ 1_code/5_regLogisticReg.R
@@ -50,3 +50,27 @@ convertTestCategoricalToBinary <- function(testData){
            HBA1C = as.numeric(HBA1C)) %>%
     select(-c(Race, Ethnicity, hypoUnaware, educationCat, bmiCat, insDoseCat))
 }
+
+convertTestCategoricalToBinary_svm <- function(testData){
+  testData %>% 
+    dplyr::select(-c(PtID, BCaseControlStatus)) %>%
+    mutate(Gender = if_else(Gender == "F", 1, 0),
+           race_white = if_else(Race == "White", 1, 0),
+           # race_asian = if_else(Race == "Asian", 1, 0),
+           race_black = if_else(Race == "Black/African American", 1, 0),
+           hispanic = if_else(Ethnicity == "Hispanic or Latino", 1, 0),
+           LiveAlone = if_else(LiveAlone == "Yes", 1, 0),
+           InsDeliveryMethod = if_else(InsDeliveryMethod == "Pump", 1, 0),
+           hypo_reducedAwareness = if_else(hypoUnaware == "reduced awareness", 1, 0),
+           hypo_unaware = if_else(hypoUnaware == "unaware", 1, 0),
+           college = if_else(educationCat == "College", 1, 0),
+           advDegree = if_else(educationCat == "Advanced degree", 1, 0),
+           underweight = if_else(bmiCat == "underweight", 1, 0),
+           overweight = if_else(bmiCat == "overweight", 1, 0),
+           obese = if_else(bmiCat == "obese", 1, 0),
+           insDose_40to60 = if_else(insDoseCat == "40to60", 1, 0),
+           insDose_greater60 = if_else(insDoseCat == "greater60", 1, 0),
+           HBA1C = as.numeric(HBA1C)) %>%
+    select(-c(Race, Ethnicity, hypoUnaware, educationCat, bmiCat, insDoseCat))
+}
+
